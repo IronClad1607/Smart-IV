72d44a32e15c15f64ba1c2ad499832ed55b03349
Ishaan Jain <ishaan160799@gmail.com>
2020-04-11 14:57:04 +0530

    Splash Activity done

--- mobile app/app/src/main/java/com/codeslayers/smartiv/LoginActivity.kt
+++ mobile app/app/src/main/java/com/codeslayers/smartiv/LoginActivity.kt
@@ -3,10 +3,10 @@ package com.codeslayers.smartiv
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
---
+++ mobile app/app/src/main/java/com/codeslayers/smartiv/SplashActivity.kt
@@ -0,0 +1,35 @@
+package com.codeslayers.smartiv
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+
+    private var mDelayHandler: Handler? = null
+    private var SPLASH_DELAY: Long = 5000
+
+    private val mRunnable: Runnable = Runnable {
+        if (!isFinishing) {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mDelayHandler = Handler()
+        mDelayHandler!!.postDelayed(mRunnable, SPLASH_DELAY)
+    }
+
+    override fun onDestroy() {
+        if (mDelayHandler != null) {
+            mDelayHandler!!.removeCallbacks(mRunnable)
+        }
+        super.onDestroy()
+    }
+}

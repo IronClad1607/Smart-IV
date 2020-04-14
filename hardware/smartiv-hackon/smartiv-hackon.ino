be0454396dee7f73d8161faebd1bb10cffdff09a
sidhi-sid <sidhisidh469@gmail.com>
2020-04-15 00:44:09 +0530

    hardware:connecting nodemcu to wifi

--- hardware/smartiv-hackon/smartiv-hackon.ino
+++ hardware/smartiv-hackon/smartiv-hackon.ino
@@ -9,6 +9,10 @@ int ldr=A0;
 #define FIREBASE_HOST "smart-iv-hackon.firebaseio.com"
 #define FIREBASE_AUTH ""
 
+//Credentials for connection to Wi-Fi
+#define WIFI_SSID "Galaxy"
+#define WIFI_PASSWORD "ssingh9970"
+
 void setup() {
   // put your setup code here, to run once:
 

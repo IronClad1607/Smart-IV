9ccf5ed7c191d92f2e31d9d08bc7011c5f8be3ba
Ishaan Jain <ishaan160799@gmail.com>
2020-04-14 14:08:13 +0530

    new Entry Form complete

--- mobile app/app/src/main/java/com/codeslayers/smartiv/ui/CreateEntryActivity.kt
+++ mobile app/app/src/main/java/com/codeslayers/smartiv/ui/CreateEntryActivity.kt
@@ -2,10 +2,18 @@ package com.codeslayers.smartiv.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.codeslayers.smartiv.R
 import kotlinx.android.synthetic.main.activity_create_entry.*
+import kotlinx.android.synthetic.main.activity_create_entry.view.*
 
 class CreateEntryActivity : AppCompatActivity() {
+
+    var patDripName: String = ""
 
     val dripName = arrayOf(
         "0.9% Normal Saline",
@@ -14,12 +22,42 @@ class CreateEntryActivity : AppCompatActivity() {
         "Dextrose in Saline",
         "Dextrose in LP",
         "0.45% Normal Saline",
-        "Glucose"
+        "Glucose",
+        "None"
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_entry)
+
+        etDripSpec.visibility = View.GONE
+
+        spinner.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dripName)
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                Toast.makeText(
+                    this@CreateEntryActivity,
+                    "Please Select A IV Fluid!",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                patDripName = dripName[position]
+                if (patDripName == "None") {
+                    etDripSpec.visibility = View.VISIBLE
+                }else{
+                    etDripSpec.visibility = View.GONE
+                }
+            }
+
+        }
 
 
         btnSubmit.setOnClickListener {
@@ -31,8 +69,24 @@ class CreateEntryActivity : AppCompatActivity() {
             val patAge = etPatAge.editText?.text.toString()
             val patBG = etPatBG.editText?.text.toString()
             val patDis = etPatDis.editText?.text.toString()
-            val patSpecialDrip = "None"
             val emergencyNumber = etPatEN.editText?.text.toString()
+            if(etDripSpec.visibility == View.VISIBLE){
+                patDripName = etDripSpec.editText?.text.toString()
+            }
+
+
+            Log.d("PUI","""
+                $patName
+                $patGen
+                $roomNumber
+                $bedNumber
+                $consultingDoctor
+                $patAge
+                $patBG
+                $patDis
+                $emergencyNumber
+                $patDripName
+            """.trimIndent())
         }
     }
 }

b612553e2b3dce525a81144a06574937074ddbf6
sidhi-sid <sidhisidh469@gmail.com>
2020-04-15 16:29:05 +0530

    hardware: connect firebase with nodemcu webserver: import bootstrap

--- hardware/smartiv-hackon/smartiv-hackon.ino
+++ hardware/smartiv-hackon/smartiv-hackon.ino
@@ -1,3 +1,14 @@
+int ldr=A0;
+//Libraries needed
+#include <ESP8266WiFi.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+#include <FirebaseArduino.h>
+
+//Credentials for connection to firebase
+#define FIREBASE_HOST "smart-iv-hackon.firebaseio.com"
+#define FIREBASE_AUTH ""
+
 void setup() {
   // put your setup code here, to run once:
 

d008f073b2ac5ec70a2a78013323061bd3ac1778
Ishaan Jain <ishaan160799@gmail.com>
2020-04-16 20:23:08 +0530

    android:create new entry COMPLETE

--- mobile app/app/src/main/java/com/codeslayers/smartiv/createentry/PatientDetailActivity.kt
+++ mobile app/app/src/main/java/com/codeslayers/smartiv/createentry/PatientDetailActivity.kt
@@ -1,17 +1,40 @@
 package com.codeslayers.smartiv.createentry
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import com.codeslayers.smartiv.R
+import com.codeslayers.smartiv.model.PatDetails
+import com.codeslayers.smartiv.ui.HomeActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_patient_detail.*
 import kotlinx.android.synthetic.main.cv_drip_spinner.*
 import kotlinx.android.synthetic.main.cv_patient_details.*
 import kotlinx.android.synthetic.main.cv_patient_medical.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.DisposableHandle
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class PatientDetailActivity : AppCompatActivity() {
+
+    private var mDelayHandler: Handler? = null
+    private var DELAY: Long = 1000
+
+    private val dbDrip by lazy {
+        FirebaseDatabase.getInstance("https://smart-iv-hackon.firebaseio.com/")
+    }
+
+
+    private val nurseUser by lazy {
+        FirebaseAuth.getInstance().currentUser
+    }
 
     var patDripName: String = ""
 
@@ -26,22 +49,29 @@ class PatientDetailActivity : AppCompatActivity() {
         "None"
     )
 
+    private val mRunnable: Runnable = Runnable {
+        if (!isFinishing) {
+            val backIntent =
+                Intent(this@PatientDetailActivity, HomeActivity::class.java)
+            startActivity(backIntent)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_patient_detail)
 
-        var patID = intent.getStringExtra("patID")!!
-        var patName = intent.getStringExtra("patName")!!
-        var patGen = intent.getStringExtra("patGen")!!
-        var patPN = intent.getStringExtra("patPN")!!
-        var patAge = intent.getStringExtra("patAge")!!
-        var patBG = intent.getStringExtra("patBG")!!
-        var patEN = intent.getStringExtra("patEN")!!
-        var patDoc = intent.getStringExtra("patDoc")!!
-        var patSym = intent.getStringExtra("patSym")!!
-        var roomNumber = intent.getStringExtra("roomNum")!!
-        var bedNumber = intent.getStringExtra("bedNum")!!
+        val patID = intent.getStringExtra("patID")!!
+        val patName = intent.getStringExtra("patName")!!
+        val patGen = intent.getStringExtra("patGen")!!
+        val patPN = intent.getStringExtra("patPN")!!
+        val patAge = intent.getStringExtra("patAge")!!
+        val patBG = intent.getStringExtra("patBG")!!
+        val patEN = intent.getStringExtra("patEN")!!
+        val patDoc = intent.getStringExtra("patDoc")!!
+        val patSym = intent.getStringExtra("patSym")!!
+        val roomNumber = intent.getStringExtra("roomNum")!!
+        val bedNumber = intent.getStringExtra("bedNum")!!
 
         runOnUiThread {
             tvPatID.text = patID
@@ -82,7 +112,47 @@ class PatientDetailActivity : AppCompatActivity() {
                     etDripSpec.visibility = View.GONE
                 }
             }
-
         }
+
+        btnDripSubmit.setOnClickListener {
+            if (etDripSpec.visibility == View.VISIBLE) {
+                patDripName = etDripSpec.editText?.text.toString()
+            }
+            val dripStatus: Boolean = false
+
+            val child = PatDetails(
+                patName,
+                patDoc,
+                patID,
+                nurseUser?.email.toString(),
+                patBG,
+                patSym,
+                patDripName,
+                dripStatus
+            )
+
+            GlobalScope.launch(Dispatchers.Main) {
+                val myRef = dbDrip.reference
+
+                val dripAdd = myRef.child("Room Number $roomNumber").child("Bed Number $bedNumber")
+                dripAdd.setValue(child)
+                    .addOnSuccessListener {
+                        runOnUiThread {
+                            btnDripSubmit.doResult(true)
+                        }
+
+                        mDelayHandler = Handler()
+                        mDelayHandler!!.postDelayed(mRunnable, DELAY)
+
+                    }
+            }
+        }
+
+    }
+
+
+    override fun onBackPressed() {
+        val backIntent = Intent(this, HomeActivity::class.java)
+        startActivity(backIntent)
     }
 }
--- mobile app/app/src/main/java/com/codeslayers/smartiv/model/PatDetails.kt
+++ mobile app/app/src/main/java/com/codeslayers/smartiv/model/PatDetails.kt
@@ -2,12 +2,11 @@ package com.codeslayers.smartiv.model
 
 data class PatDetails(
     val PatientName: String,
-    val PatientGender: String,
     val ConsultingDoctor: String,
-    val PatientAge: String,
+    val PatientID: String,
+    val NurseID: String,
     val PatientBloodGroup: String,
-    val PatientDisease: String,
-    val PatientEN: String,
+    val PatientSymptoms: String,
     val PatientIVFluid: String,
     val DripStatus: Boolean
 )

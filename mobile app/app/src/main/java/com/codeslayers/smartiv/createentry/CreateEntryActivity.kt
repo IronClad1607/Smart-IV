3c7784c67556125db7ac630a91bf10dc40059375
Ishaan Jain <ishaan160799@gmail.com>
2020-04-16 17:52:48 +0530

    android:couroutines added

--- mobile app/app/src/main/java/com/codeslayers/smartiv/createentry/CreateEntryActivity.kt
+++ mobile app/app/src/main/java/com/codeslayers/smartiv/createentry/CreateEntryActivity.kt
@@ -3,16 +3,32 @@ package com.codeslayers.smartiv.createentry
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.codeslayers.smartiv.R
 import com.codeslayers.smartiv.ui.HomeActivity
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_create_entry.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class CreateEntryActivity : AppCompatActivity() {
+
+    var patName: String = ""
+    var patGen: String = ""
+    var patPN: String = ""
+    var patAge: String = ""
+    var patBG: String = ""
+    var patEn: String = ""
+    var patDoc: String = ""
+    var patSym: String = ""
+    var roomNumber: String = ""
+    var bedNumber: String = ""
+
+
     private val db by lazy {
         FirebaseDatabase.getInstance("https://smart-iv-hackon-patientid.firebaseio.com/")
     }
@@ -25,17 +41,43 @@ class CreateEntryActivity : AppCompatActivity() {
         btnPatID.setOnClickListener {
             val patID = etPatientID.editText?.text.toString()
 
-            val myRef = db.reference
-            myRef.addValueEventListener(object : ValueEventListener {
-                override fun onCancelled(p0: DatabaseError) {
-                    btnPatID.doResult(false)
-                }
+            GlobalScope.launch(Dispatchers.Main) {
+                val myRef = db.reference
+                myRef.addValueEventListener(object : ValueEventListener {
+                    override fun onCancelled(p0: DatabaseError) {
+                        btnPatID.doResult(false)
+                    }
 
-                override fun onDataChange(p0: DataSnapshot) {
+                    override fun onDataChange(p0: DataSnapshot) {
+                        showData(p0, patID)
+                        runOnUiThread {
+                            btnPatID.doResult(true)
+                        }
 
-                }
 
-            })
+                        Log.d(
+                            "PUI", """
+                        $patName
+                        $patGen
+                        $patPN
+                    """.trimIndent()
+                        )
+                    }
+
+                })
+            }
+        }
+    }
+
+    private fun showData(snapshot: DataSnapshot, patID: String) {
+        for (ds in snapshot.children) {
+            if (ds.key == patID) {
+                patName = ds.child("patientName").value.toString()
+                patGen = ds.child("patientGender").value.toString()
+                patPN = ds.child("patientMobile").value.toString()
+                patAge = ds.child("patientAge").value.toString()
+                patBG = ds.child("patientBloodGroup").value.toString()
+            }
         }
     }
 
